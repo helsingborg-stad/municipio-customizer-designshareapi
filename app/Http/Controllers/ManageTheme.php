8732c142bf44a79b54ca6f627c0180a0163d35fe
Sebastian Thulin <sebastianthulin@users.noreply.github.com>
2021-03-25 15:37:19 +0100

    Update ManageTheme.php
    Adds a simplified and more efficient lookup for isPublic determination

--- app/Http/Controllers/ManageTheme.php
+++ app/Http/Controllers/ManageTheme.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Storage;
 
 class ManageTheme extends Controller
 {
-    private $storage = 'themes'; 
+    private $storage = 'themes';
 
     /**
      * Get all files stored in themes folder
@@ -25,16 +25,16 @@ class ManageTheme extends Controller
 
                 $items[] = [
                     'id' => isset($content->id) ? $content->id : null,
-                    'name' => isset($content->name) ? $content->name : null 
+                    'name' => isset($content->name) ? $content->name : null
                 ];
             }
-            return Response::json($items, 200); 
+            return Response::json($items, 200);
         }
 
         return Response::json(array(
             'code'      =>  500,
-            'message'   =>  "Could not find any themes." 
-        ), 500); 
+            'message'   =>  "Could not find any themes."
+        ), 500);
     }
 
     /**
@@ -48,14 +48,14 @@ class ManageTheme extends Controller
         if(!Storage::disk('local')->has($this->storage . "/" . $id . ".json")) {
             return Response::json(array(
                 'code'      =>  404,
-                'message'   =>  "Theme does not exists." 
-            ), 404); 
+                'message'   =>  "Theme does not exists."
+            ), 404);
         }
 
         return Response::json(
             json_decode(
                 Storage::get($this->storage . "/" . $id . ".json")
-        ), 200); 
+        ), 200);
     }
 
     /**
@@ -67,16 +67,16 @@ class ManageTheme extends Controller
     public function update(Request $request) {
 
         //Validate client secret
-        $clientSecret = $request->header('CLIENT-SITE-ID'); 
+        $clientSecret = $request->header('CLIENT-SITE-ID');
         if(strlen($clientSecret) != 32) {
             return Response::json(array(
                 'code'      =>  400,
-                'message'   =>  "Header 'CLIENT-SITE-ID' is not valid, please proide a string containing 32 chars." 
-            ), 400); 
+                'message'   =>  "Header 'CLIENT-SITE-ID' is not valid, please proide a string containing 32 chars."
+            ), 400);
         }
 
         //Create a filename dependant on the client secret and app_secret
-        $fileName = $this->createInternalId($clientSecret); 
+        $fileName = $this->createInternalId($clientSecret);
 
         //Get file data
         $input = $request->only(['website', 'name', 'mods']);
@@ -84,11 +84,11 @@ class ManageTheme extends Controller
         //Append local data
         $input['public']    = $this->isPublic($input['website']);
         $input['updated']   = date('Y-m-d H:i:s');
-        $input['id']        = $fileName; 
+        $input['id']        = $fileName;
 
         //Store file on disk
         Storage::disk('local')->put(
-            $this->storage . '/' . $fileName . '.json', 
+            $this->storage . '/' . $fileName . '.json',
             json_encode($input),
             'private'
         );
@@ -97,7 +97,7 @@ class ManageTheme extends Controller
         return Response::json(array(
             'code'      =>  200,
             'message'   =>  'Record updated with data.',
-            'data'      =>  $input 
+            'data'      =>  $input
         ), 200);
     }
 
@@ -107,17 +107,18 @@ class ManageTheme extends Controller
      * @return boolean
      */
     private function isPublic($url) {
-        try {
-            $client     = new \GuzzleHttp\Client(['verify' => false]);
-            $response   = $client->get($url);
 
-            if($response->getStatusCode() == 200) {
-                return true;
-            }
-        } catch(Error $e) {
-            return false; 
+        $hostname = parse_url($url, PHP_URL_HOST);
+
+        if ((bool) dns_get_record($hostname, DNS_A)) {
+            return true;
         }
-        return false; 
+
+        if ((bool) dns_get_record($hostname, DNS_CNAME)) {
+            return true;
+        }
+
+        return false;
     }
 
     /**
@@ -130,13 +131,13 @@ class ManageTheme extends Controller
 
         //Create key
         if(getenv('APP_SECRET', true)) {
-            return md5($key . getenv('APP_SECRET')); 
+            return md5($key . getenv('APP_SECRET'));
         }
 
         //Log error
-        error_log("No private APP_SECRET enviroment variable defined. The system works without it, but should be considered insecure."); 
+        error_log("No private APP_SECRET enviroment variable defined. The system works without it, but should be considered insecure.");
 
         //Create key
-        return md5($key); 
+        return md5($key);
     }
 }
